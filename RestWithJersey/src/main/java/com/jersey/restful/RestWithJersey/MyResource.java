1845ff9f89798b675b1613b48d5edbd22c25b044
Dhirendra Singh <dhiirendra8@gmail.com>
2018-09-10 02:58:49 +0530

    cc

--- RestWithJersey/src/main/java/com/jersey/restful/RestWithJersey/MyResource.java
+++ RestWithJersey/src/main/java/com/jersey/restful/RestWithJersey/MyResource.java
@@ -20,7 +20,7 @@ public class MyResource {
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String getIt() {
-        return "This is my resourse";
+        return "This is my resourse  ";
         
     }
 }
